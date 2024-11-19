5723169997bf351f7ad2478d576784f9cd171722
Duo. <155233908+heilcheng@users.noreply.github.com>
2024-11-19 23:24:11 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DevFestDrawer from './DevFest-Drawer';
+
+function App() {
+  return (
+    <div className="App">
+      <DevFestDrawer />
+    </div>
+  );
+}
+
+export default App;
